e1c35a9ea207e31f94a98bca5a6468971aed37f4
isomrodm6911 <43758333+isomrodm6911@users.noreply.github.com>
2018-10-03 13:46:33 -0400

    Add files via upload

---
+++ P2HW1_CelsiusConverter_MarissaIsom-Rodriguez.py
@@ -0,0 +1,19 @@
+# Celsius Fahrenheit Converter
+# 9/24/18
+# CTI-110 P2HW1- Celsius Fahrenheit Converter
+# Marissa Isom-Rodriguez
+
+ #Display Celsius Temperatur to Fahrenheit Formula
+print ("F = 9/5 C + 32")
+
+# Convert Celsius to Fahrenheit
+
+Celsius = int(input("Enter a temperature in Celsius:"))
+
+#Fahrenheit Formula
+
+Fahrenheit = 9.0/5.0 * Celsius + 32
+
+# Final : Fahrenheit = ?
+print ('Temperature:',Celsius,'Celsius =',Fahrenheit,'F')
+
